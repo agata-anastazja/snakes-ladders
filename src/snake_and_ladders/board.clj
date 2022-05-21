d06b9e97a3e6a7e0d08cae6456db4d3fbac26512
agata-anastazja <agata.sumowska@signal-ai.com>
2022-05-21 11:36:04 +0100

    create a board with snakes

--- src/snake_and_ladders/board.clj
+++ src/snake_and_ladders/board.clj
@@ -2,3 +2,14 @@
 
 (defn initialise-empty-board []
   (vec (repeat 100 {})))
+
+(defn apply-snake [board snake]
+  (update board (first snake) (fn [_] {:snake snake})))
+
+(defn apply-snakes [board snakes]
+  (reduce (fn [acc snake] (apply-snake acc snake)) board snakes ))
+
+(defn initialise-board [snakes ladders]
+  (->
+   (initialise-empty-board)
+   (apply-snakes snakes)))
--- test/snake_and_ladders/board_test.clj
+++ test/snake_and_ladders/board_test.clj
@@ -8,6 +8,23 @@
             you get an empty board with a 100 fields represented by empty dictionaries"
     (let [result (initialise-empty-board)]
       (is (= 100 (count result)))
-      (is (every? #(= % {}) result)))))
+      (is (every? #(= % {}) result))))
+  (testing "given a list of snakes with one snake
+            when you initialise the board
+            you get a configured board with a snake"
+    (let [snakes [[2 1]]
+          ladders [[]]
+          result (initialise-board snakes ladders)]
+      (is (= 100 (count result)))
+      (is (= (nth result 2) {:snake [2 1]}))))
+  (testing "given a list of snakes with 2 snakes
+            when you initialise the board
+            you get a configured board with 2 snakes"
+    (let [snakes [[2 1] [4 3]]
+          ladders [[]]
+          result (initialise-board snakes ladders)]
+      (is (= 100 (count result)))
+      (is (= (nth result 2) {:snake [2 1]}))
+      (is (= (nth result 4) {:snake [4 3]})))))
 
 
